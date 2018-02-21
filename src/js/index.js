a0d0da1f186f32d6fda99aeba498edad17a892b9
Maciej Myśliński <maciej.myslinski@netguru.pl>
2018-02-21 02:31:32 +0100

    Barbone reveal.js presentation

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,5 @@
-console.log('Hello from js');
+import Reveal from "reveal.js";
+import 'reveal.js/css/reveal.css';
+import 'reveal.js/css/theme/solarized.css';
+
+Reveal.initialize({ dependencies: [] });
