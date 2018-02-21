934fdc381b4d5a9739f80eff54ad1c49fdc176a6
Maciej Myśliński <maciej.myslinski@netguru.pl>
2018-02-21 02:47:51 +0100

    redo presentation with react

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,31 @@
 import Reveal from "reveal.js";
-import 'reveal.js/css/reveal.css';
-import 'reveal.js/css/theme/solarized.css';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import { injectGlobal } from 'styled-components';
+import "reveal.js/css/reveal.css";
+import "reveal.js/css/theme/solarized.css";
 
-Reveal.initialize({ dependencies: [] });
+injectGlobal`
+  #root {
+    height: 100vh;
+  }
+`
+
+class App extends Component {
+  componentDidMount() {
+    Reveal.initialize({ dependencies: [] });
+  }
+
+  render() {
+    return (
+      <div className="reveal">
+        <div className="slides">
+          <section>Slide 1</section>
+          <section>Slide 2</section>
+        </div>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById("root"));
